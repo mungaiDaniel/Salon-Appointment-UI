5a36a9041f18f66686b4d58da163d6964eaea327
mungaiDaniel <danitomonga@gmail.com>
2023-02-02 11:26:41 +0300

    salon services

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Header from './components/Header';
 import Login from './components/Login';
 import Pict from './components/Pict';
 import Footer from './components/Footer';
+import Home from './components/Home';
 import Register from './components/Register';
+import Services from './components/Services';
 import {Button, Alert, Row, Col} from 'react-bootstrap';
 import 'font-awesome/css/font-awesome.min.css';
 import React, {useState} from 'react';
+
 function App() {
   const [currentForm, setCurrentForm] = useState('register');
+  const [homePage, setHomePage] = useState('home')
 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
@@ -20,14 +24,22 @@ function App() {
 
       <Header />
 
-      <Row className='landing p-lg-0 pt-lg-5'>
+      { 
+        homePage === 'home' ? <Home /> :
+        <Row className='landing p-lg-0 pt-lg-5'>
         <Col>{
           currentForm === "login" ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
         }</Col>
         <Col> <Pict/></Col>
       </Row>
+      }
+      <Services />
+      
       <br/>
-
+      <br/>
+      <br/>
+      <br/>
+      <br/>
       <Footer />
     </div>
   );
