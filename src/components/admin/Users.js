7cb1cd49f09de7f16b43771c4ea20aafd6cce2ab
mungaiDaniel <danitomonga@gmail.com>
2023-02-17 12:56:59 +0300

    Booking details

---
+++ src/components/admin/Users.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { Avatar, Box , Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@material-ui/core'
+import { AccountBalance, Person } from '@material-ui/icons'
+
+const user = [	{
+    "Email": "Danitomonga@gmail.com",
+    "FirstName": "Daniel",
+    "Id": 1,
+    "LastName": "Mungai",
+    "Location": "Uthiru, Waiyaki way",
+    "Password": "12345",
+    "PhoneNumber": 727980611
+},
+{
+    "Email": "Peninahmukiri@gmail.com",
+    "FirstName": "Peninah",
+    "Id": 2,
+    "LastName": "Mukiri",
+    "Location": "muthiga, Waiyaki way",
+    "Password": "12345",
+    "PhoneNumber": 71234567
+},
+{
+    "Email": "Danitomonga@gmail.com",
+    "FirstName": "Daniel",
+    "Id": 1,
+    "LastName": "Mungai",
+    "Location": "Uthiru, Waiyaki way",
+    "Password": "12345",
+    "PhoneNumber": 727980611
+},
+{
+    "Email": "Peninahmukiri@gmail.com",
+    "FirstName": "Peninah",
+    "Id": 2,
+    "LastName": "Mukiri",
+    "Location": "muthiga, Waiyaki way",
+    "Password": "12345",
+    "PhoneNumber": 71234567
+},
+{
+    "Email": "Danitomonga@gmail.com",
+    "FirstName": "Daniel",
+    "Id": 1,
+    "LastName": "Mungai",
+    "Location": "Uthiru, Waiyaki way",
+    "Password": "12345",
+    "PhoneNumber": 727980611
+},
+{
+    "Email": "Peninahmukiri@gmail.com",
+    "FirstName": "Peninah",
+    "Id": 2,
+    "LastName": "Mukiri",
+    "Location": "muthiga, Waiyaki way",
+    "Password": "12345",
+    "PhoneNumber": 71234567
+}
+]
+
+const Users = () => {
+  return (
+    <div className='bg-light '>
+      <Box className='m-5 '>
+         <Typography variant='h5' color='warning' align='center' > 
+            My Users
+         </Typography>
+         <Box className='mx-5'>
+         <List disablePadding> 
+            {user.map((text, index)=>(
+                <>
+                <ListItem key={index} button >
+                    <ListItemAvatar>
+                        <Avatar>
+                            <Person/>
+                        </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={text.FirstName} secondary={text.PhoneNumber}>
+
+                    </ListItemText>
+                </ListItem>
+                <Divider />
+                </>
+            ))}
+         </List>
+         </Box>
+      </Box>
+    </div>
+  )
+}
+
+export default Users
